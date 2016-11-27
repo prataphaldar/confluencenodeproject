15f85829d9ec290057c992235fd2394740d8873f
prataphaldar <prataphaldar1987@gmail.com>
2016-11-27 14:17:10 +0530

    mongoconfig commit

--- server/mongo/mongodbconfig.js
+++ server/mongo/mongodbconfig.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 module.exports= function(){
-    var db='mongodb://localhost/exampleDB';
-    mongoose.connect(db);    
+    //var db='mongodb://localhost/exampleDB';
+    var db='mongodb://root:root@ds111718.mlab.com:11718/exampledb';
+    mongoose.connect(db);
+    var con = mongoose.connection;
+    con.once('open',function(){
+        console.log("connected to database successfully");
+    });
 }
